3742af9326666aba2f94cc8c71be07de1fb2af3d
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-04-23 11:07:05 +0100

    Apis

---
+++ src/Utils/Urls.js
@@ -0,0 +1,4 @@
+const REACT_APP_AUTH_BASE_URL = "http://localhost:3000/api/v1/auth"
+const REACT_APP_USER_BASE_URL = "http://localhost:3000/api/v1/user"
+
+export { REACT_APP_USER_BASE_URL, REACT_APP_AUTH_BASE_URL}
