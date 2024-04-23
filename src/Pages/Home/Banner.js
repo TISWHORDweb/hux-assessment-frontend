056a350786a24c394f6520d3659e3e5cb8dc4ba9
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-04-23 11:06:35 +0100

    Button text changes

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -12,7 +12,7 @@ function Banner() {
                     <p>Organize your contacts, share information seamlessly, and stay connected with ease. Our web application makes managing contacts a breeze. Tired of the constant struggle to keep track of your contacts? Scattered emails, outdated spreadsheets, and cluttered notes create a disorganized mess, hindering your productivity and making it difficult to find the information you need, when you need it.</p>
                     <div className="btns animate__animated animate__bounce">
                     <button class="bn632-hover bn21" type="">Try It Free</button>
-                    <button class="bn632-hover bn19" type="">Sign Up Now</button>
+                    <button class="bn632-hover bn19" type="">Explore</button>
                     </div>
                 </div>
                 <img src={BannerImg} alt="" />
